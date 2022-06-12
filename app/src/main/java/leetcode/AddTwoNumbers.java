724d999a93418b1d486407e3702ae355342aeaa7
goyand <sennkoub@gmail.com>
2022-06-12 17:26:30 +0900

    [add]AddTwoNumbers

---
+++ app/src/main/java/leetcode/AddTwoNumbers.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddTwoNumbers {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    List<Integer> results = new ArrayList<>();
+    boolean finished = false;
+    boolean rankup = false;
+    while (!finished) {
+      int val1 = l1.val;
+      int val2 = l2.val;
+
+      int sum = val1 + val2;
+      sum = rankup ? sum + 1 : sum;
+      rankup = sum >= 10;
+      results.add(sum % 10);
+
+      if (l1.next == null && l2.next == null && !rankup) {
+        finished = true;
+      }
+
+      l1 = l1.next == null ? new ListNode() : l1.next;
+      l2 = l2.next == null ? new ListNode() : l2.next;
+    }
+
+    ListNode result = new ListNode();
+    for (int i = results.size() - 1; i >= 0; i--) {
+      result = i == results.size() - 1 ? new ListNode(results.get(i)) : new ListNode(results.get(i), result);
+    }
+
+    return result;
+  }
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+}
---
+++ app/src/test/java/leetcode/AddTwoNumbersTest.java
@@ -0,0 +1,87 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import leetcode.AddTwoNumbers.ListNode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddTwoNumbersTest {
+
+  @Test
+  void example1() {
+    // setup
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+    ListNode list1_3 = addTwoNumbers.new ListNode(3);
+    ListNode list1_2 = addTwoNumbers.new ListNode(4, list1_3);
+    ListNode list1_1 = addTwoNumbers.new ListNode(2, list1_2);
+
+    ListNode list2_3 = addTwoNumbers.new ListNode(4);
+    ListNode list2_2 = addTwoNumbers.new ListNode(6, list2_3);
+    ListNode list2_1 = addTwoNumbers.new ListNode(5, list2_2);
+
+    ListNode list3_3 = addTwoNumbers.new ListNode(8);
+    ListNode list3_2 = addTwoNumbers.new ListNode(0, list3_3);
+    ListNode list3_1 = addTwoNumbers.new ListNode(7, list3_2);
+
+    ListNode actual = addTwoNumbers.addTwoNumbers(list1_1, list2_1);
+
+    assertEquals(list3_1.val, actual.val);
+    assertEquals(list3_2.val, actual.next.val);
+    assertEquals(list3_3.val, actual.next.next.val);
+  }
+
+  @Test
+  void example2() {
+    // setup
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+    ListNode list1_1 = addTwoNumbers.new ListNode(0);
+
+    ListNode list2_1 = addTwoNumbers.new ListNode(0);
+
+    ListNode list3_1 = addTwoNumbers.new ListNode(0);
+
+    ListNode actual = addTwoNumbers.addTwoNumbers(list1_1, list2_1);
+
+    assertEquals(list3_1.val, actual.val);
+  }
+
+  @Test
+  void example3() {
+    // setup
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+    ListNode list1_7 = addTwoNumbers.new ListNode(9);
+    ListNode list1_6 = addTwoNumbers.new ListNode(9, list1_7);
+    ListNode list1_5 = addTwoNumbers.new ListNode(9, list1_6);
+    ListNode list1_4 = addTwoNumbers.new ListNode(9, list1_5);
+    ListNode list1_3 = addTwoNumbers.new ListNode(9, list1_4);
+    ListNode list1_2 = addTwoNumbers.new ListNode(9, list1_3);
+    ListNode list1_1 = addTwoNumbers.new ListNode(9, list1_2);
+
+    ListNode list2_4 = addTwoNumbers.new ListNode(9);
+    ListNode list2_3 = addTwoNumbers.new ListNode(9, list2_4);
+    ListNode list2_2 = addTwoNumbers.new ListNode(9, list2_3);
+    ListNode list2_1 = addTwoNumbers.new ListNode(9, list2_2);
+
+    ListNode list3_8 = addTwoNumbers.new ListNode(1);
+    ListNode list3_7 = addTwoNumbers.new ListNode(0, list3_8);
+    ListNode list3_6 = addTwoNumbers.new ListNode(0, list3_7);
+    ListNode list3_5 = addTwoNumbers.new ListNode(0, list3_6);
+    ListNode list3_4 = addTwoNumbers.new ListNode(9, list3_5);
+    ListNode list3_3 = addTwoNumbers.new ListNode(9, list3_4);
+    ListNode list3_2 = addTwoNumbers.new ListNode(9, list3_3);
+    ListNode list3_1 = addTwoNumbers.new ListNode(8, list3_2);
+
+    ListNode actual = addTwoNumbers.addTwoNumbers(list1_1, list2_1);
+
+    assertEquals(list3_1.val, actual.val);
+    assertEquals(list3_2.val, actual.next.val);
+    assertEquals(list3_3.val, actual.next.next.val);
+    assertEquals(list3_4.val, actual.next.next.next.val);
+    assertEquals(list3_5.val, actual.next.next.next.next.val);
+    assertEquals(list3_6.val, actual.next.next.next.next.next.val);
+    assertEquals(list3_7.val, actual.next.next.next.next.next.next.val);
+    assertEquals(list3_8.val, actual.next.next.next.next.next.next.next.val);
+  }
+
+}
