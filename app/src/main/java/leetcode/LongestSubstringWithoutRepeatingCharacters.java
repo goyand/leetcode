b6ceb3969b15e9e5b99ec103a986a1abcc8ab6d0
goyand <sennkoub@gmail.com>
2022-06-18 17:35:21 +0900

    [add]LongestSubstringWithoutRepeatingCharacters_a

--- app/src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ app/src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,89 +1,21 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
-  private static final List<String> PATTERNS = new ArrayList<>(
-      Arrays.asList("(", ")", "*", "+", "?", "[", "]", "\\", "{", "}"));
-
   public int lengthOfLongestSubstring(String s) {
-    // 1. 文字列の途中で、2つのcharに挟まれており、重複する文字列がない
-    // 2. 文頭からcharの重複を許さずに取得できる最長文字列
-    // 3. 文末からcharの重複を許さずに取得できる最長文字列
-
-    // change string to char list.
-    Set<Character> chars = s.chars().mapToObj(item -> (char) item).collect(Collectors.toSet());
-
-    int maxLength = 0;
-
-    // 1.
-    for (Character c : chars) {
-      String letter = Character.toString(c);
-      if (PATTERNS.contains(letter)) {
-        letter = "\\" + Character.toString(c);
+    int n = s.length(), ans = 0;
+    Map<Character, Integer> map = new HashMap<>(); // current index of character
+    // try to extend the range [i, j]
+    for (int j = 0, i = 0; j < n; j++) {
+      if (map.containsKey(s.charAt(j))) {
+        i = Math.max(map.get(s.charAt(j)), i);
       }
-
-      maxLength = Math.max(maxLength, longestUniqueWordCountBetweenChars(letter, s));
+      ans = Math.max(ans, j - i + 1);
+      map.put(s.charAt(j), j + 1);
     }
-
-    List<Character> charList = s.chars().mapToObj(item -> (char) item).collect(Collectors.toList());
-    // 2.
-    maxLength = Math.max(maxLength, longestFromLeft(charList));
-    // 3.
-    maxLength = Math.max(maxLength, longestFromRight(charList));
-
-    return maxLength;
-  }
-
-  public int longestFromLeft(List<Character> characters) {
-    Set<Character> charSet = new HashSet<>();
-    for (int i = 0; i < characters.size(); i++) {
-      if (charSet.contains(characters.get(i))) {
-        break;
-      }
-      charSet.add(characters.get(i));
-    }
-    return charSet.size();
-  }
-
-  public int longestFromRight(List<Character> characters) {
-    Set<Character> charSet = new HashSet<>();
-    for (int i = characters.size() - 1; i >= 0; i--) {
-      if (charSet.contains(characters.get(i))) {
-        break;
-      }
-      charSet.add(characters.get(i));
-    }
-    return charSet.size();
-  }
-
-  public int longestUniqueWordCountBetweenChars(String letter, String str) {
-    List<String> splitWords = Arrays.asList(str.split(letter));
-    if (splitWords.size() == 0) {
-      return 1;
-    }
-
-    int maxLength = 1;
-    for (int i = 0; i < splitWords.size(); i++) {
-      List<Character> wordChars = splitWords.get(i).chars().mapToObj(item -> (char) item).collect(Collectors.toList());
-      if (i != 0) {
-        maxLength = Math.max(maxLength, longestFromLeft(wordChars) + 1);
-      }
-      if (i != splitWords.size() - 1) {
-        maxLength = Math.max(maxLength, longestFromRight(wordChars) + 1);
-      }
-      if (wordChars.size() != wordChars.stream().distinct().count()) {
-        // 重複チェック
-        continue;
-      }
-      maxLength = Math.max(maxLength, wordChars.size() + 1);
-    }
-    return maxLength;
+    return ans;
   }
 }
